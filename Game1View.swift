899ee6e8d15a59688119570d5f96b3bc9d5640dd
Jessica Akemi Meguro <jessicameguro@gmail.com>
2022-04-22 15:30:01 -0300

    Adicionar som

--- Game1View.swift
+++ Game1View.swift
@@ -5,16 +5,20 @@
 //  Created by Jessica Akemi Meguro on 22/04/22.
 //
 
+import AVFoundation
 import Foundation
 import SwiftUI
+
+var player: AVAudioPlayer!
 
 struct Game1View: View {
     
     @State var imageArray: [(imageName: String, position: CGPoint, isImageVisible: Bool)] = [("sacola1", CGPoint(x: UIScreen.main.bounds.width * 4.3 / 5, y: UIScreen.main.bounds.height * 3.5 / 5), true), ("tampa1", CGPoint(x: UIScreen.main.bounds.width * 3.2 / 5, y: UIScreen.main.bounds.height * 4.2 / 5), true), ("tampa2", CGPoint(x: UIScreen.main.bounds.width * 3.2 / 5, y: UIScreen.main.bounds.height * 2.5 / 5), true ), ("bottle1", CGPoint(x: UIScreen.main.bounds.width * 4.5 / 5, y: UIScreen.main.bounds.height * 2.5 / 5), true), ("bottle2", CGPoint(x: UIScreen.main.bounds.width * 1.5 / 5, y: UIScreen.main.bounds.height * 3.1 / 5), true), ("food1", CGPoint(x: UIScreen.main.bounds.width * 2.9 / 5, y: UIScreen.main.bounds.height * 2.9 / 5), true)
-        ]
+    ]
     
     @State var isDragging = false
     @State var position = CGSize.zero
+
     
     var body: some View {
         
@@ -37,40 +41,30 @@ struct Game1View: View {
                         .onTapGesture {
                             withAnimation {
                                 imageArray[index].isImageVisible.toggle()
+                                playSound()
                             }
                         }
                     
                 }
-                
-                
-//                Image("sacola1")
-//                    .renderingMode(.original)
-//                    .resizable()
-//                    .opacity(isImageVisible ? 1 : 0)
-//                    .animation(.linear)
-//                    .frame(width: geo.size.width * 0.3, height: geo.size.height * 0.2)
-//                    .offset(x: position.width, y: position.height)
-//                    .position(x: UIScreen.main.bounds.width * 3.2 / 5, y: UIScreen.main.bounds.height * 4.2 / 5)
-//                    .onTapGesture {
-//                        withAnimation {
-//                            isImageVisible.toggle()
-//                        }
-//                    }
-                
-//                Button {
-//                    print("Edit button was tapped")
-//                } label: {
-//                    Image("tampa1")
-//                        .renderingMode(.original)
-//                        .resizable()
-//                        .offset(x: position.width, y: position.height)
-//                }
-//                .frame(width: geo.size.width * 0.07, height: geo.size.height * 0.06)
-//                .position(x: UIScreen.main.bounds.width * 4.3 / 5, y: UIScreen.main.bounds.height * 3 / 5)
             }
         }
         .ignoresSafeArea()
         
+    }
+}
+
+func playSound() {
+    let url = Bundle.main.url(forResource: "plasticsound", withExtension: "mp3")
+    
+    guard url != nil else {
+        return
+    }
+    
+    do {
+        player = try AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    } catch {
+        print("error")
     }
 }
 
