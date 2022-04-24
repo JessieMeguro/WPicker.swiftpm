fae9b9870276816b813835f7ad2cf4bc937cd424
Jessica Akemi Meguro <jessicameguro@gmail.com>
2022-04-23 21:53:15 -0300

    Appearing completed game 1

--- Game1View.swift
+++ Game1View.swift
@@ -11,6 +11,23 @@ import SwiftUI
 
 var player: AVAudioPlayer!
 
+struct ContinueButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        ZStack {
+            Image("buttonStart")
+                .scaleEffect(configuration.isPressed ? 0.8 : 0.8)
+            //                    .opacity(configuration.isPressed ? 1 : 08)
+                .animation(.default, value: configuration.isPressed)
+            Text("Continue the day")
+                .padding(.bottom)
+                .font(.custom("Inter-Bold", size: 40))
+                .foregroundColor(.black)
+                .opacity(0.9)
+        }
+    }
+}
+
 struct Game1View: View {
     
     @State var imageArray: [(imageName: String, position: CGPoint, isImageVisible: Bool)] = [("sacola1", CGPoint(x: UIScreen.main.bounds.width * 4.3 / 5, y: UIScreen.main.bounds.height * 3.5 / 5), true), ("tampa1", CGPoint(x: UIScreen.main.bounds.width * 3.2 / 5, y: UIScreen.main.bounds.height * 4.2 / 5), true), ("tampa2", CGPoint(x: UIScreen.main.bounds.width * 3.2 / 5, y: UIScreen.main.bounds.height * 2.5 / 5), true ), ("bottle1", CGPoint(x: UIScreen.main.bounds.width * 4.5 / 5, y: UIScreen.main.bounds.height * 2.5 / 5), true), ("bottle2", CGPoint(x: UIScreen.main.bounds.width * 1.5 / 5, y: UIScreen.main.bounds.height * 3.1 / 5), true), ("food1", CGPoint(x: UIScreen.main.bounds.width * 2.9 / 5, y: UIScreen.main.bounds.height * 2.9 / 5), true)
@@ -24,7 +41,9 @@ struct Game1View: View {
         UINavigationBar.setAnimationsEnabled(false)
     }
     
+    @State var canNavigate: Bool = false
     @State private var isImageVisible = true
+    @State private var fadeIn = false
     
     var body: some View {
         
@@ -46,33 +65,33 @@ struct Game1View: View {
             }
             .position(x: UIScreen.main.bounds.width * 1.9 / 5, y: UIScreen.main.bounds.height * 0.55 / 5)
             .rotationEffect(.degrees(-11))
-                
-                ZStack {
-                    Image("jornal4")
-                        .resizable()
-                        .frame(width: 500, height: 125)
-                        .opacity(isImageVisible ? 0 : 1)
-                        .animation(.spring().delay(4), value: isImageVisible)
-                    
-                    Text("Help him pick up the plastic trash.")
-                        .padding(.bottom)
-                        .font(.custom("Inter-SemiBold", size: 24))
-                        .foregroundColor(.black)
-                        .multilineTextAlignment(.center)
-                        .opacity(isImageVisible ? 0 : 1)
-                        .animation(.spring().delay(4), value: isImageVisible)
-                }
-                .onAppear {
-                    isImageVisible = false
-                }
-                .position(x: UIScreen.main.bounds.width * 2 / 5, y: UIScreen.main.bounds.height * 0.9 / 5)
-                .rotationEffect(.degrees(-11))
             
-//            ZStack {
-//                Image("jornal2")
-//                    .resizable()
-//                    .frame(width: 520, height: 202)
-//            }
+            ZStack {
+                Image("jornal4")
+                    .resizable()
+                    .frame(width: 500, height: 125)
+                    .opacity(isImageVisible ? 0 : 1)
+                    .animation(.spring().delay(4), value: isImageVisible)
+                
+                Text("Help him pick up the plastic trash.")
+                    .padding(.bottom)
+                    .font(.custom("Inter-SemiBold", size: 24))
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .opacity(isImageVisible ? 0 : 1)
+                    .animation(.spring().delay(4), value: isImageVisible)
+            }
+            .onAppear {
+                isImageVisible = false
+            }
+            .position(x: UIScreen.main.bounds.width * 2 / 5, y: UIScreen.main.bounds.height * 0.9 / 5)
+            .rotationEffect(.degrees(-11))
+            
+            //            ZStack {
+            //                Image("jornal2")
+            //                    .resizable()
+            //                    .frame(width: 520, height: 202)
+            //            }
             
             
             GeometryReader { geo in
@@ -95,41 +114,62 @@ struct Game1View: View {
                         }
                 }
                 if allRemoved {
-                    VStack {
-                        //                        Image("jornalTela1")
-                        NavigationLink(destination: Game2View()) {
-                            Text("Botão")
-                                .background(.green)
-                                .zIndex(10)
+                    ZStack {
+                        
+                        VStack {
+                            Image("completedGame1")
+                                .resizable()
+                                .scaledToFill()
+                                .zIndex(20)
+                                .onAppear() {
+                                    withAnimation(Animation.easeIn(duration: 0.6)) {
+                                        fadeIn.toggle()
+                                    }
+                                }.opacity(fadeIn ? 1 : 0)
                         }
+                        
+                        NavigationLink(destination: Game2View(), isActive: $canNavigate) {
+                            Button("") {
+                                self.canNavigate = true
+                            } .buttonStyle(ContinueButton())
+                        }
+                        .frame(width: 200, height: 220)
+                        .position(x: UIScreen.main.bounds.width * 2.5 / 5, y: UIScreen.main.bounds.height * 4.5 / 5)
+                        .zIndex(21)
+                        
                     }
+                    //                            Text("Botão")
+                    //                                .padding(.bottom)
+                    //                                .font(.custom("Inter-SemiBold", size: 24))
+                    //                                .foregroundColor(.black)
+                    //                                .multilineTextAlignment(.center)
                 }
             }
+            .ignoresSafeArea()
+            .navigationBarHidden(true)
+            .navigationBarBackButtonHidden(true)
+            
         }
-        .ignoresSafeArea()
-        .navigationBarHidden(true)
-        .navigationBarBackButtonHidden(true)
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "plasticSound", withExtension: "mp3")
         
+        guard url != nil else {
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url!)
+            player?.play()
+        } catch {
+            print("error")
+        }
     }
-}
-
-func playSound() {
-    let url = Bundle.main.url(forResource: "plasticSound", withExtension: "mp3")
     
-    guard url != nil else {
-        return
-    }
-    
-    do {
-        player = try AVAudioPlayer(contentsOf: url!)
-        player?.play()
-    } catch {
-        print("error")
-    }
-}
-
-struct Game1View_Previews: PreviewProvider {
-    static var previews: some View {
-        Game1View()
+    struct Game1View_Previews: PreviewProvider {
+        static var previews: some View {
+            Game1View()
+        }
     }
 }
